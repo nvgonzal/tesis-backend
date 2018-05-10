f360b8ecc84b16c795961ea144f2c55d0b22bde3
Nicolás González <nikolasgonzalez8@gmail.com>
2018-05-10 14:00:38 -0300

    Crea migracion y modelo de empresa

---
+++ database/migrations/2018_05_10_164221_create_empresas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmpresasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('empresas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre');
+            $table->string('razon_social');
+            $table->string('rut');
+            $table->string('direccion');
+            $table->string('cuenta_pago');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('empresas');
+    }
+}
