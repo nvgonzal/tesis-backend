9f9596895422affb32c2b28ce6042bccf2a7e426
Nicolas Gonzalez <nikolasgonzalez8@gmail.com>
2018-07-30 02:39:21 -0400

    Agrega comportamiento para manejar credenciales de usuario incorrectas

--- app/Http/Controllers/LoginProxy.php
+++ app/Http/Controllers/LoginProxy.php
@@ -3,27 +3,35 @@
 namespace App\Http\Controllers;
 
 
+use GuzzleHttp\Exception\ClientException;
 use Illuminate\Validation\UnauthorizedException;
+use URL;
 
 class LoginProxy
 {
     public static function attemptLogin($email,$password){
         $client = new \GuzzleHttp\Client();
-        $response = $client->post(env('APP_URL').'/oauth/token'
-            ,['form_params' => [
-                'grant_type'    => 'password',
-                'client_id'     => env('PASSWORD_CLIENT_ID'),
-                'client_secret' => env('PASSWORD_CLIENT_SECRET'),
-                'username'      => $email,
-                'password'      => $password,
-                'grant'         => '*'
-            ]
-            ]);
+        try {
+            $response = $client->post(URL::to('/oauth/token')
+                , ['form_params' => [
+                    'grant_type' => 'password',
+                    'client_id' => env('PASSWORD_CLIENT_ID'),
+                    'client_secret' => env('PASSWORD_CLIENT_SECRET'),
+                    'username' => $email,
+                    'password' => $password,
+                    'grant' => '*'
+                ]
+                ]);
+        } catch (ClientException $e) {
+            $data = ['message' => 'Tus credenciales son incorrectas' , 'status' => '401'];
+            return $data;
+        }
 
         $responseBody = \GuzzleHttp\json_decode($response->getBody());
         $data = [
             'access_token'  => $responseBody->access_token,
-            'expires_in'    => $responseBody->expires_in
+            'expires_in'    => $responseBody->expires_in,
+            'status'        => '200'
         ];
         return $data;
     }
